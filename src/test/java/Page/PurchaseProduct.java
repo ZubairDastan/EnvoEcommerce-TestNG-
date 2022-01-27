1d501e380ea66e960e7946cfa22bfef35b218f51
Zubair Hasan <zubairhasan007@gmail.com>
2022-01-27 15:46:14 +0600

    Little work

--- src/test/java/Page/PurchaseProduct.java
+++ src/test/java/Page/PurchaseProduct.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 
 import java.util.List;
 
@@ -18,27 +19,26 @@ public class PurchaseProduct {
     List<WebElement> viewCart;
     @FindBy(xpath = "//a[contains(text(),\"Proceed to checkout\")]")
     WebElement btnCheckout;
-    @FindBy(name = "product_cat")
+    @FindBy(id = "pa_color")
     WebElement dropdown;
-    @FindBy(className = "fa-search")
-    WebElement searchIcon;
+    @FindBy(className = "entry-title")
+    WebElement prodName;
 
     public PurchaseProduct(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
 
-    public void selectProduct() throws InterruptedException {
+    public void matchName() throws InterruptedException {
         imgProducts.get(1).click();
         Thread.sleep(2000);
-        Select select = new Select(dropdown);
-        select.selectByIndex(3);
-        searchIcon.click();
-        Thread.sleep(2000);
-        imgProducts.get(1).click();
+        String name = prodName.getText();
+        Assert.assertEquals("Black pants", name);
     }
 
-    public void addCart() throws InterruptedException {
+    public void selectColor() throws InterruptedException {
+        Select select = new Select(dropdown);
+        select.selectByIndex(1);
         Thread.sleep(2000);
         btnAddCart.click();
         Thread.sleep(2000);
--- src/test/java/TestRunner/PurchaseTestRunner.java
+++ src/test/java/TestRunner/PurchaseTestRunner.java
@@ -7,14 +7,15 @@ import org.testng.annotations.Test;
 public class PurchaseTestRunner extends EnvironmentSetup {
     @Test(priority = 1, groups = "purchase", description = "Select a product")
     public void selectMyProduct() throws InterruptedException {
-        driver.get("https://envothemes.com/envo-ecommerce");
+        driver.get("https://envothemes.com/envo-ecommerce/shop/");
         PurchaseProduct purchase = new PurchaseProduct(driver);
-        purchase.selectProduct();
+        purchase.matchName();
+
     }
 
     @Test(priority = 2, groups = "purchase", description = "Add product in cart")
     public void addCart() throws InterruptedException {
         PurchaseProduct purchase = new PurchaseProduct(driver);
-        purchase.addCart();
+        purchase.selectColor();
     }
 }
